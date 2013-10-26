a5273edbf79797f16db2050041b2d160fcad9760
Vegard Gillestad <vegard.gillestad@gmail.com>
2013-10-26 22:41:30 +0200

    Refactored away the use of filters + the view get the settings injected + theme can now be used as base

--- AdminMenu.cs
+++ AdminMenu.cs
@@ -2,36 +2,51 @@
 using Orchard.Themes.Services;
 using Orchard.UI.Navigation;
 
-namespace Bootstrap {
-    public class AdminMenu : INavigationProvider {
+namespace Bootstrap
+{
+    public class AdminMenu : INavigationProvider
+    {
         private readonly ISiteThemeService _siteThemeService;
 
         public Localizer T { get; set; }
         public string MenuName { get { return "admin"; } }
 
-        public AdminMenu(ISiteThemeService siteThemeService) {
+        public AdminMenu(ISiteThemeService siteThemeService)
+        {
             _siteThemeService = siteThemeService;
         }
 
-        public void GetNavigation(NavigationBuilder builder) {
-            var themeName = _siteThemeService.GetSiteTheme();
-            if (themeName.Name == Constants.ThemeName) {
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            if (IsBootstrapThemeOrBasedOnBootstrapTheme())
+            {
                 builder.AddImageSet("themes")
                     .Add(T("Themes"), "10", BuildMenu);
             }
         }
 
-        private void BuildMenu(NavigationItemBuilder menu) {
+        private void BuildMenu(NavigationItemBuilder menu)
+        {
             menu.Add(T(""), "10.1",
                 item => item
                     .Action("Index", "Admin", new { area = Constants.RoutesAreaName })
-                    .Permission(Bootstrap.Permissions.ManageThemeSettings)
+                    .Permission(Permissions.ManageThemeSettings)
             );
             menu.Add(T("Choose Options"), "10.2",
                 item => item
                     .Action("Index", "Admin", new { area = Constants.RoutesAreaName })
-                    .Permission(Bootstrap.Permissions.ManageThemeSettings)
+                    .Permission(Permissions.ManageThemeSettings)
             );
+        }
+
+        private bool IsBootstrapThemeOrBasedOnBootstrapTheme()
+        {
+            var theme = _siteThemeService.GetSiteTheme();
+            if (theme.Name == Constants.ThemeName || theme.BaseTheme == Constants.ThemeName)
+                return true;
+            if (theme.Name.Contains(Constants.ThemeName)) //Should find a way I could recursivly check if Bootstrap is the base theme.
+                return true;
+            return false;
         }
     }
 }
